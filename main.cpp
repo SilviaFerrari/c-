f6b001c3bb9543e5018e6a72accf0f5a09dae835
silvia <40138805+SilviaFerrari@users.noreply.github.com>
2019-09-30 23:07:33 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <windows.h>
 #include "void.cpp"
 using namespace std;
 int main()
@@ -13,34 +14,41 @@ int main()
 	cout << "Ti diamo il benvenuto nella tua rubrica personale." << endl
 	<< "Premi un tasto qualunque per visualizzare i tuoi contatti." << endl;
 	cin >> key;
+	main_page();
 	do
 	{
-		main_page();
 		cin >> key;
 		cout << endl;
-		
 		switch(key)
 		{
 			case 'a':
 				add_contact();
 				break;
-			
 			case 's':
 				search();
 				break;
-			
-			case 'r':
-				remove();
-				break;
-			
+			case 'd':
+				open_contact();
+				cin >> key;
+				switch(key)
+				{
+					case 'c':
+						change();
+						break;						
+					case 'r':
+						remove();
+						break;							
+					case 'm':
+						main_page();
+						break;
+				} 		
+				break;	
 			case 'e':
 				remove_all();
 				break;
-			
 			case 'q':
 				quit = true;
 				break;
 		}
-		
 	}while(quit == false); 	
 }
